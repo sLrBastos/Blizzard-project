7c279664ee3eca0b017289738244daba47dabb7a
sLrBastos <cerejobastosa7x@gmail.com>
2023-06-20 18:44:16 +0100

    access token and mount request working POG

--- src/components/Oauth.jsx
+++ src/components/Oauth.jsx
@@ -2,39 +2,51 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const CLIENT_ID = "bbdd3352857a4f75832b4c5e3d0b5774" 
-const CLIENT_SECRET = "10aWFwwTwouNsocO8vruhxG0RXqQhnDY"
-
+const CLIENT_ID = "bbdd3352857a4f75832b4c5e3d0b5774";
+const CLIENT_SECRET = "10aWFwwTwouNsocO8vruhxG0RXqQhnDY";
 
 const Oauth = () => {
+  const [accessToken, setAccessToken] = useState("");
 
-const [acessToken, setAccessToken] = useState("")
+  const getAccessToken = () => {
+    const options = {
+      method: "post",
+      url: "https://oauth.battle.net/token",
+      params: {
+        grant_type: "client_credentials",
+      },
+      auth: {
+        username: CLIENT_ID,
+        password: CLIENT_SECRET,
+      },
+    };
+    axios(options).then((res) => setAccessToken(res.data.access_token));
+  };
 
-   const getAccessToken = () => {
-        axios.post("https://oauth.battle.net/token"),
-        new URLSearchParams({
-            'grant_type': 'client_credentials'
-          }),
-          {
-            auth: {
-              username: CLIENT_ID,
-              password: CLIENT_SECRET
-            }
-          }
-          .then((response) => {
-            setAccessToken(response.data)
-          })
-   }
+  const getMounts = () => {
+    const options = {
+      method: "get",
+      url: "https://eu.api.blizzard.com/data/wow/mount/index",
+      params: {
+        ":region": "eu",
+        namespace: "static-eu",
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    axios(options).then((res) => console.log(res));
+  };
 
-   useEffect(() => {
-    getAccessToken()
-   }, [])
-    
-  return (
-    <div>Oauth</div>
+  useEffect(() => {
+    getAccessToken();
+  }, []);
 
+  useEffect(() => {
+    getMounts();
+  }, [accessToken]);
 
-  )
-}
+  return <div>Oauth</div>;
+};
 
-export default Oauth
+export default Oauth;
