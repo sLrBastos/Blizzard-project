50445926bc97659e480d5bbf175d744c75003a95
sLrBastos <cerejobastosa7x@gmail.com>
2023-07-12 16:23:29 +0100

    Mounts and their hrefs keys separated into 2 different tables via json.parse. very cool

--- blizzard-server/database/beans.js
+++ blizzard-server/database/beans.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
 require("dotenv").config();
-const mysql = require('mysql2');
+const fs = require("fs");
+const mysql = require("mysql2");
+
+
 // const mounts = require("../../SQL/mounts.data.json")
 
 // Read the JSON file
-const jsonData = fs.readFileSync("../SQL/mounts.data.json");
+const jsonData = fs.readFileSync("SQL/mounts.data.json");
 const data = JSON.parse(jsonData);
 
 // Separate the data into two sets based on your logic
@@ -12,18 +14,19 @@ const dataSet1 = data.map(item => ({ name: item.name, id: item.id }));
 const dataSet2 = data.map(item => ({ href: item.key.href }));
 
 // Create a MySQL connection
-const { DB_HOST, DB_USER, DB_PASSWORD} = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 const connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
+    database: DB_NAME,
 });
 
 // Insert data from the first set into the first table
 dataSet1.forEach((data) => {
-  const query = "INSERT INTO mounts (name, id) VALUES (?, ?)";
+  const things = "INSERT INTO mounts (name, id) VALUES (?, ?)";
   const values = [data.name, data.id];
-  connection.query(query, values, (err) => {
+  connection.query(things, values, (err) => {
     if (err) {
       console.error(err);
     }
